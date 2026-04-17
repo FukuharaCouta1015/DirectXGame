f07d76332893a9a241cfdb57a797607c82ad81cf
FukuharaCouta1015 <k024g0088@g.neec.ac.jp>
2026-04-17 12:30:16 +0900

    確認用

---
+++ GameScene.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "KamataEngine.h"
+
+class GameScene {
+	
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
+#include "KamataEngine.h"
 #include <Windows.h>
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE3D_21_フクハラ_コウタ");
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// メインループ
+	while (true) {
+
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		dxCommon->PreDraw();
+
+		dxCommon->PostDraw();
+
+
+	}
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
 	return 0;
 }
